796f96a7b73635bbbd9498e3378b60e4ad139556
zhouch <457588502@qq.com>
2019-03-19 11:18:23 +0800

    first coommit

---
+++ app/src/main/java/nshine/com/nsdashcam/MainActivity.java
@@ -0,0 +1,13 @@
+package nshine.com.nsdashcam;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
